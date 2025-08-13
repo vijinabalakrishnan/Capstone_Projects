677345b046811fdae182688db5db507bbc056f5c
vijinabalakrishnan <vijinabalakrishnan@gmail.com>
2025-08-13 22:36:33 +0530

    API Testing project
    API Testing project

---
+++ API_Testing_Contact_List_Application/src/test/java/RestAPITesting.java
@@ -0,0 +1,221 @@
+package test.java;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAPITesting {
+	 private String token;
+	 private String contactId;
+	 
+    //@Test --Uncomment to register a new user
+    public void testAddNewUser() {
+        RestAssured.baseURI = "https://thinking-tester-contact-list.herokuapp.com";
+
+        String requestBody = """
+                {
+                   "firstName": "Test",
+					"lastName": "User123",
+					"email": "test_email125@fake.com",
+					"password": "myPassword"
+                }
+                """;
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+        .when()
+                .post("/users")
+        .then()
+                .statusCode(201)
+                .statusLine(equalTo("HTTP/1.1 201 Created"))
+                .extract().response();
+        
+        String token = response.jsonPath().getString("token");
+        
+        token.codePoints();
+    }
+    
+    @BeforeClass
+    public void setupAndLogin() {
+        RestAssured.baseURI = "https://thinking-tester-contact-list.herokuapp.com";
+
+        // Login to get token (assumes user already exists)
+        String loginBody = """
+                {
+                    "email": "test_email125@fake.com",
+                    "password": "myPassword"
+                }
+                """;
+
+        Response loginResponse = given()
+                .contentType(ContentType.JSON)
+                .body(loginBody)
+        .when()
+                .post("/users/login")
+        .then()
+                .statusCode(200)
+                .extract().response();
+
+        token = loginResponse.jsonPath().getString("token");
+    }
+    
+    //@Test
+    public void testGetUserProfile() {
+        given()
+                .header("Authorization", "Bearer " + token)
+        .when()
+                .get("/users/me")
+        .then()
+                .statusCode(200)
+                .statusLine(equalTo("HTTP/1.1 200 OK"));
+    }
+    
+    //@Test
+    public void testUpdateUser() {
+        String updateBody = """
+                {
+                    "firstName": "Updated",
+                    "lastName": "Username",
+                    "email": "test_email125@fake.com",
+					"password": "myPassword"
+                }
+                """;
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(updateBody)
+        .when()
+                .patch("/users/me")
+        .then()
+                .statusCode(200)
+                .statusLine(equalTo("HTTP/1.1 200 OK"));
+    }
+    
+    //@Test
+    public void testAddContact() {
+        String contactBody = """
+                {
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "birthdate": "1970-01-01",
+                    "email": "test_email125@fake.com",
+                    "phone": "8005555555",
+                    "street1": "1 Main St.",
+                    "street2": "Apartment A",
+                    "city": "Anytown",
+                    "stateProvince": "KS",
+                    "postalCode": "12345",
+                    "country": "USA"
+                }
+                """;
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(contactBody)
+        .when()
+                .post("/contacts")
+        .then()
+                .statusCode(201)
+                .statusLine(equalTo("HTTP/1.1 201 Created"));
+    }
+    
+    @Test
+    public void testGetContactList() {
+    	Response searchResponse = given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+        .when()
+                .get("/contacts")
+        .then()
+                .statusCode(200)
+                .statusLine(equalTo("HTTP/1.1 200 OK"))
+                .extract().response();
+
+    	contactId = searchResponse.jsonPath().getString("[0]._id");
+    	System.out.println("First Contact ID: " + contactId);
+    }
+    
+    @Test
+    public void testGetContactById() {
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+        .when()
+                .get("/contacts/" + contactId)
+        .then()
+                .statusCode(200)
+                .statusLine(equalTo("HTTP/1.1 200 OK"));
+    }
+    
+    @Test
+    public void testUpdateContact() {
+        String updateBody = """
+                {
+                    "firstName": "Amy",
+                    "lastName": "Miller",
+                    "birthdate": "1992-02-02",
+                    "email": "test_email125@fake.com",
+                    "phone": "8005554242",
+                    "street1": "13 School St.",
+                    "street2": "Apt. 5",
+                    "city": "Washington",
+                    "stateProvince": "QC",
+                    "postalCode": "A1A1A1",
+                    "country": "Canada"
+                }
+                """;
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(updateBody)
+        .when()
+                .put("/contacts/" + contactId)
+        .then()
+                .statusCode(200)
+                .body("email", equalTo("test_email125@fake.com"))
+                .statusLine(equalTo("HTTP/1.1 200 OK"));
+    }
+    
+    @Test
+    public void testPatchUpdateContact() {
+        String patchBody = """
+                {
+                    "firstName": "Anna"
+                }
+                """;
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+                .body(patchBody)
+        .when()
+                .patch("/contacts/" + contactId)
+        .then()
+                .statusCode(200)
+                .body("firstName", equalTo("Anna"))
+                .statusLine(equalTo("HTTP/1.1 200 OK"));
+    }
+    
+    @Test
+    public void testLogoutUser() {
+        given()
+                .header("Authorization", "Bearer " + token)
+                .contentType(ContentType.JSON)
+        .when()
+                .post("/users/logout")
+        .then()
+                .statusCode(200)
+                .statusLine(equalTo("HTTP/1.1 200 OK"));
+    }
+}
